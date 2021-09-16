23a45ddbd634b213387998241966256b639237ad
Zeljko <cereus87@gmail.com>
2021-09-17 00:34:31 +0200

    Inserted successfully first data in products database

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -62,7 +62,7 @@
                                 </div>
                             </div> <!-- form group for Product Category end -->
                             <div class="form-group">
-                                <label class="col-md-3 control-label">Categories</label>
+                                <label class="col-md-3 control-label">Category</label>
                                 <div class="col-md-6">
                                     <select name="category" class="form-control">
                                         <option> Select a Category </option>
@@ -131,3 +131,38 @@
         </div> <!-- row end -->
     </body>
 </html>
+
+<?php
+    if(isset($_POST['submit']))
+    {
+        $product_title = $_POST['product_title'];
+        $product_category = $_POST['product_category'];
+        $category = $_POST['category'];
+        $product_price = $_POST['product_price'];
+        $product_keywords = $_POST['product_keywords'];
+        $product_desc = $_POST['product_desc'];
+
+        // product images
+        $product_img1 = $_FILES['product_img1']['name'];
+        $product_img2 = $_FILES['product_img2']['name'];
+        $product_img3 = $_FILES['product_img3']['name'];
+
+        $temp_name1 = $_FILES['product_img1']['tmp_name'];
+        $temp_name2 = $_FILES['product_img2']['tmp_name'];
+        $temp_name3 = $_FILES['product_img3']['tmp_name'];
+
+        move_uploaded_file($temp_name1, "product_images/$product_img1");
+        move_uploaded_file($temp_name2, "product_images/$product_img2");
+        move_uploaded_file($temp_name3, "product_images/$product_img3");
+
+        $insert_product = "insert into products (p_category_id,	category_id, date, product_title, product_img1, product_img2, product_img3, product_price, product_keywords, product_desc)
+        values ('$product_category', '$category', NOW(), '$product_title', '$product_img1', '$product_img2', '$product_img3', '$product_price', '$product_keywords', '$product_desc')";
+
+        $run_product = mysqli_query($conn, $insert_product);
+        if($run_product)
+        {
+            echo "<script>alert('Product has been inserted successfully')</script>";
+            echo "<script>window.open('insert_product.php', '_self')</script>";
+        }
+    }
+?>
