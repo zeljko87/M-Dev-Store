a1225488da852b11c192144a4b436db3b564b371
zeljko87 <cereus87@gmail.com>
2021-09-23 15:04:02 -0700

    Partitaly done Working with Customer Orders

--- payment_options.php
+++ payment_options.php
@@ -1,8 +1,17 @@
 
 <div class="box">
+
+    <?php
+        $session_email = $_SESSION['customer_email'];
+        $select_customer = "select * from customers where customer_email='$session_email'";
+        $run_customer = mysqli_query($conn, $select_customer);
+        $row_customer = mysqli_fetch_array($run_customer);
+        $customer_id = $row_customer['customer_id'];
+    ?>
+
     <h1 class="text-center"> Payment Options for You </h1>
     <p class="lead text-center">
-        <a href="#"> Offline Payment </a>
+        <a href="order.php?c_id=<?php echo $customer_id; ?>"> Offline Payment </a>
     </p>
     <center>
         <p class="lead">
