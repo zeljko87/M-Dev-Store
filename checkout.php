4276d22adb8da4ba1b171ebc4be16ba3c1427559
zeljko87 <cereus87@gmail.com>
2021-09-22 22:07:04 -0700

    Initiated login.php and checkout.php

---
+++ checkout.php
@@ -0,0 +1,24 @@
+<?php
+    $active = "Register";
+    include("includes/header.php");
+?>
+        <div id="content">
+            <div class="container">
+                <div class="col-md-12">
+                    <ul class="breadcrumb">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="customer_register.php">Register</a></li>
+                    </ul>
+                </div> <!-- col-md-12 end -->
+                <div class="col-md-3">
+                    <?php
+                        include("includes/sidebar.php");
+                     ?>
+                </div> <!-- col-md-3 end -->
+            </div> <!-- containter end -->
+        </div> <!-- content end -->
+            <?php
+                include("includes/footer.php");
+            ?>
+        </body>
+    </html>
