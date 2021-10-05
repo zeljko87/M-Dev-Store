23a53539823326f2a4dc73a044b63dadf6583277
Zeljko <cereus87@gmail.com>
2021-10-05 19:52:32 +0200

    Admin Panel 1 - Created nav part

--- admin_area/includes/sidebar.php
+++ admin_area/includes/sidebar.php
@@ -1,28 +1,150 @@
-<div class="panel panel-default sidebar-menu">
-    <div class="panel-heading">
-        <h3 class="panel-title">Products Categories</h3>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar-ex1-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="index.php?dashboard" class="navbar-brand">Admin Area</a>
     </div>
-    <div class="panel-body">
-        <ul class="nav nav-pills nav-stacked category-menu">
-            <li><a href="">Jackets</a></li>
-            <li><a href="">Accessories</a></li>
-            <li><a href="">Shoes</a></li>
-            <li><a href="">Coats</a></li>
-            <li><a href="">T-Shirt</a></li>
-        </ul>
+    <ul class="nav navbar-right top-nav">
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <i class="fa fa-user"></i> Mr Zex <b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a href="index.php?user_profile">
+                        <i class="fa fa-fw fa-user"></i> Profile
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?view_products">
+                        <i class="fa fa-fw fa-envelope"></i> Products
+                        <span class="badge">7</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?view_customers">
+                        <i class="fa fa-fw fa-users"></i> Customers
+                        <span class="badge">11</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?view_cats">
+                        <i class="fa fa-fw fa-gear"></i> Product Categories
+                        <span class="badge">4</span>
+                    </a>
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <a href="logout.php">
+                        <i class="fa fa-fw fa-power-off"></i> Log Out
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul> <!-- nav navbar-right top-nav finish -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav side-nav">
+            <li>
+                <a href="index.php?dashboard">
+                    <i class="fa fa-fw fa-dashboard"></i> Dashboard
+                </a>
+            </li>
+            <li>
+                <a href="#" data-toggle="collapse" data-target="#products">
+                    <i class="fa fa-fw fa-tag"></i> Products
+                    <i class="fa fa-fw fa-caret-down"></i>
+                </a>
+                <ul id="products" class="collapse">
+                    <li>
+                        <a href="index.php?insert_product"> Insert Product </a>
+                    </li>
+                    <li>
+                        <a href="index.php?view_products"> View Products </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" data-toggle="collapse" data-target="#p_cat">
+                    <i class="fa fa-fw fa-edit"></i> Product Categories
+                    <i class="fa fa-fw fa-caret-down"></i>
+                </a>
+                <ul id="p_cat" class="collapse">
+                    <li>
+                        <a href="index.php?insert_p_cat"> Insert Product Category </a>
+                    </li>
+                    <li>
+                        <a href="index.php?view_p_cats"> View Product Categories </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" data-toggle="collapse" data-target="#cat">
+                    <i class="fa fa-fw fa-book"></i> Categories
+                    <i class="fa fa-fw fa-caret-down"></i>
+                </a>
+                <ul id="cat" class="collapse">
+                    <li>
+                        <a href="index.php?insert_cat"> Insert Category </a>
+                    </li>
+                    <li>
+                        <a href="index.php?view_cats"> View Categories </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" data-toggle="collapse" data-target="#slides">
+                    <i class="fa fa-fw fa-gear"></i> Slides
+                    <i class="fa fa-fw fa-caret-down"></i>
+                </a>
+                <ul id="slides" class="collapse">
+                    <li>
+                        <a href="index.php?insert_slide"> Insert Slide </a>
+                    </li>
+                    <li>
+                        <a href="index.php?view_slides"> View Slides </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="index.php?view_customers">
+                    <i class="fa fa-fw fa-users"></i> View Customers
+                </a>
+            </li>
+            <li>
+                <a href="index.php?view_orders">
+                    <i class="fa fa-fw fa-book"></i> View Orders
+                </a>
+            </li>
+            <li>
+                <a href="index.php?view_payments">
+                    <i class="fa fa-fw fa-money"></i> View Payments
+                </a>
+            </li>
+            <li>
+                <a href="#" data-toggle="collapse" data-target="#users">
+                    <i class="fa fa-fw fa-users"></i> Users
+                    <i class="fa fa-fw fa-caret-down"></i>
+                </a>
+                <ul id="users" class="collapse">
+                    <li>
+                        <a href="index.php?insert_user"> Insert User </a>
+                    </li>
+                    <li>
+                        <a href="index.php?view_users"> View Users </a>
+                    </li>
+                    <li>
+                        <a href="index.php?user_profile"> Edit User Profile </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="logout.php">
+                    <i class="fa fa-fw fa-power-off"></i> Log Out
+                </a>
+            </li>
+        </ul> <!-- nav navbar-nav side-nav end -->
     </div>
-</div> <!-- panel panel-default sidebar-menu end -->
-
-<div class="panel panel-default sidebar-menu">
-    <div class="panel-heading">
-        <h3 class="panel-title">Categories</h3>
-    </div>
-    <div class="panel-body">
-        <ul class="nav nav-pills nav-stacked category-menu">
-            <li><a href="">Men</a></li>
-            <li><a href="">Women</a></li>
-            <li><a href="">Kids</a></li>
-            <li><a href="">Others</a></li>
-        </ul>
-    </div>
-</div> <!-- panel panel-default sidebar-menu end -->
+</nav>
---
+++ admin_area/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device=width, initial-scale=1">
+        <title>M-Dev Store</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <?php
+                include("includes/sidebar.php");
+            ?>
+            <div id="page-wrapper">
+                <div class="container-fluid">
+
+                </div>
+            </div> <!--
+        </div> <!-- end wrapper -->
+    </body>
+</html>
